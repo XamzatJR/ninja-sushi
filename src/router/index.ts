82429a6cc97e1b7dbe87b504e640ef2acc4413db
XamzatJR <bahaev.hamzat95@gmail.com>
2023-03-06 00:49:15 +0300

    feat: add basic routes and views

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
+const routes = [
+  { path: '/', name: 'home', component: () => import('@/views/Home.vue') },
+  { path: '/about', name: 'about', component: () => import('@/views/About.vue') }
+]
 
 const router = createRouter({
+  routes,
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
 })
 
 export default router
