36de93861e5f8c73b3780e849f1e5aef9015417b
XamzatJR <bahaev.hamzat95@gmail.com>
2023-03-08 00:56:18 +0300

    feat: add new color palette

--- windi.config.ts
+++ windi.config.ts
@@ -3,7 +3,18 @@ import formsPlugin from 'windicss/plugin/forms'
 
 export default defineConfig({
   darkMode: 'class',
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        ninja: {
+          100: '#686870',
+          200: '#F5F5F5',
+          300: '#D2D2D7',
+          400: '#FF6633'
+        }
+      }
+    }
+  },
   extract: {
     exclude: ['node_modules/**/*', '.git/**/*'],
     include: ['index.html', 'src/**/*.{vue,html,jsx,tsx}'],
