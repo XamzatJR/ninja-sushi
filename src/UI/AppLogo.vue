cf2c135d6f0e275be46630e087886950e66f1d95
XamzatJR <bahaev.hamzat95@gmail.com>
2023-03-08 23:53:50 +0300

    feat: add link to main page

---
+++ src/UI/AppLogo.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <router-link to="/" class="flex gap-2.5 px-6 cursor-pointer">
+    <img src="/logo.svg" alt="">
+    <img src="/logo-text.png" alt="">
+  </router-link >
+</template>
+
+<style scoped>
+
+</style>
