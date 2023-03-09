2083b49254174661bad680f67624cff7d64784c8
XamzatJR <bahaev.hamzat95@gmail.com>
2023-03-09 18:16:02 +0300

    feat: remove paddings

--- src/UI/AppLogo.vue
+++ src/UI/AppLogo.vue
@@ -3,9 +3,9 @@
 </script>
 
 <template>
-  <router-link to="/" class="flex gap-2.5 px-6 cursor-pointer">
-    <img src="/logo.svg" alt="">
-    <img src="/logo-text.png" alt="">
+  <router-link to="/" class="flex gap-2.5 cursor-pointer">
+    <img src="/logo.svg" alt="Ninja Sushi Logo">
+    <img src="/logo-text.png" alt="Ninja Sushi Logo">
   </router-link >
 </template>
 
