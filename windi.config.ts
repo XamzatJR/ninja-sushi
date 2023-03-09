a199eb31c602f5ed2093a77b00bd0ad1594d527e
XamzatJR <bahaev.hamzat95@gmail.com>
2023-03-09 19:33:53 +0300

    feat: add new colors to palette and chang font family value

--- windi.config.ts
+++ windi.config.ts
@@ -1,18 +1,28 @@
 import { defineConfig } from 'windicss/helpers'
 import formsPlugin from 'windicss/plugin/forms'
-
+import defaultTheme from 'windicss/defaultTheme'
 export default defineConfig({
   darkMode: 'class',
   theme: {
     extend: {
       colors: {
         ninja: {
+          50: '#F5F5F7',
+          60: '#686870',
+          69: '#9E9E9E',
+          70: '#1D1D1F',
           100: '#686870',
           200: '#F5F5F5',
           300: '#D2D2D7',
           400: '#FF6633'
         }
+      },
+      fontSize: {
+        '4.5xl': ['42px', '64px']
       }
+    },
+    fontFamily: {
+      sans: ['Inter', ...defaultTheme.fontFamily.sans]
     }
   },
   extract: {
