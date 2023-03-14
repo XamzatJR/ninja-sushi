bd9f4270daadc443ebf318016efb1994b26e9c77
XamzatJR <bahaev.hamzat95@gmail.com>
2023-03-15 01:00:28 +0300

    style: change double quotes to single

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import TheFooter from "@/components/TheFooter.vue"
-import TheHeader from "@/components/TheHeader.vue"
+import TheFooter from '@/components/TheFooter.vue'
+import TheHeader from '@/components/TheHeader.vue'
 </script>
 
 <template>
