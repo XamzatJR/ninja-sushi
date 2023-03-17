158c1eb795687d50b9493648dd879c2daec090d4
XamzatJR <bahaev.hamzat95@gmail.com>
2023-03-18 00:53:33 +0300

    refactor: delete UI component

--- src/UI/AppIcon.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div class="hover:(bg-ninja-200 border-ninja-200 text-ninja-400) active:(bg-ninja-400 text-white border-ninja-400) p-4 rounded-xl border border-ninja-300 flex justify-center items-center text-ninja-100">
-        <slot name="title" class="font-medium text-base" />
-        <slot />
-    </div>
-</template>
-
-<script setup lang="ts"></script>
-<!-- font-family: 'Inter' -->
