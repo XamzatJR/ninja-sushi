194a872072a477eeb03af6a5f1691b933b486b8c
XamzatJR <bahaev.hamzat95@gmail.com>
2023-03-21 23:33:37 +0300

    feat: make component responsive

--- src/components/ProductCategories.vue
+++ src/components/ProductCategories.vue
@@ -8,40 +8,40 @@ import IconSauce from '@/components/icons/IconSauce.vue'
 </script>
 
 <template>
-  <ul class="mx-auto mt-4 mb-8 py-1.5 px-4 flex justify-between items-center bg-white rounded-xl w-max">
-    <li class="w-[91px] py-1.5 cursor-pointer flex flex-col items-center justify-center group hover:(shadow-sm rounded-xl transition-shadow)">
-      <IconRoll class="fill-ninja-60 group-hover:fill-ninja-400" />
-      <div class="text-base font-medium text-ninja-70">
+  <ul class="mx-auto my-2 sm:(mt-4 mb-8 px-4 bg-white) py-1.5 flex justify-between items-center rounded-xl w-max <sm:(w-full gap-1 overflow-x-scroll scrollbar-hide justify-start px-1)">
+    <li class="<sm:(bg-white rounded-xl) sm:w-[91px] p-3 sm:py-1.5 cursor-pointer flex flex-col items-center justify-center group hover:(shadow-sm rounded-xl transition-shadow)">
+      <IconRoll class="fill-ninja-60 group-hover:fill-ninja-400 <sm:(w-[2.375rem] h-[2.375rem])" />
+      <div class="<sm:hidden text-base font-medium text-ninja-70">
         Роллы
       </div>
     </li>
-    <li class="w-[91px] py-1.5 cursor-pointer flex flex-col items-center justify-center group hover:(shadow-sm rounded-xl transition-shadow)">
-      <IconSushi class="fill-ninja-60 group-hover:fill-ninja-400" />
-      <div class="text-base font-medium text-ninja-70">
+    <li class="<sm:(bg-white rounded-xl) sm:w-[91px] p-3 sm:py-1.5 cursor-pointer flex flex-col items-center justify-center group hover:(shadow-sm rounded-xl transition-shadow)">
+      <IconSushi class="fill-ninja-60 group-hover:fill-ninja-400 <sm:(w-[2.375rem] h-[2.375rem])" />
+      <div class="<sm:hidden text-base font-medium text-ninja-70">
         Суши
       </div>
     </li>
-    <li class="w-[91px] py-1.5 cursor-pointer flex flex-col items-center justify-center group hover:(shadow-sm rounded-xl transition-shadow)">
-      <IconSet class="fill-ninja-60 group-hover:fill-ninja-400" />
-      <div class="text-base font-medium text-ninja-70">
+    <li class="<sm:(bg-white rounded-xl) sm:w-[91px] p-3 sm:py-1.5 cursor-pointer flex flex-col items-center justify-center group hover:(shadow-sm rounded-xl transition-shadow)">
+      <IconSet class="fill-ninja-60 group-hover:fill-ninja-400 <sm:(w-[2.375rem] h-[2.375rem])" />
+      <div class="<sm:hidden text-base font-medium text-ninja-70">
         Сеты
       </div>
     </li>
-    <li class="w-[91px] py-1.5 cursor-pointer flex flex-col items-center justify-center group hover:(shadow-sm rounded-xl transition-shadow)">
-      <IconBowl class="fill-ninja-60 group-hover:fill-ninja-400" />
-      <div class="text-base font-medium text-ninja-70">
+    <li class="<sm:(bg-white rounded-xl) sm:w-[91px] p-3 sm:py-1.5 cursor-pointer flex flex-col items-center justify-center group hover:(shadow-sm rounded-xl transition-shadow)">
+      <IconBowl class="fill-ninja-60 group-hover:fill-ninja-400 <sm:(w-[2.375rem] h-[2.375rem])" />
+      <div class="<sm:hidden text-base font-medium text-ninja-70">
         Боулы
       </div>
     </li>
-    <li class="w-[91px] py-1.5 cursor-pointer flex flex-col items-center justify-center group hover:(shadow-sm rounded-xl transition-shadow)">
-      <IconDrink class="fill-ninja-60 group-hover:fill-ninja-400" />
-      <div class="text-base font-medium text-ninja-70">
+    <li class="<sm:(bg-white rounded-xl) sm:w-[91px] p-3 sm:py-1.5 cursor-pointer flex flex-col items-center justify-center group hover:(shadow-sm rounded-xl transition-shadow)">
+      <IconDrink class="fill-ninja-60 group-hover:fill-ninja-400 <sm:(w-[2.375rem] h-[2.375rem])" />
+      <div class="<sm:hidden text-base font-medium text-ninja-70">
         Напитки
       </div>
     </li>
-    <li class="w-[91px] py-1.5 cursor-pointer flex flex-col items-center justify-center group hover:(shadow-sm rounded-xl transition-shadow)">
-      <IconSauce class="fill-ninja-60 group-hover:fill-ninja-400" />
-      <div class="text-base font-medium text-ninja-70">
+    <li class="<sm:(bg-white rounded-xl) sm:w-[91px] p-3 sm:py-1.5 cursor-pointer flex flex-col items-center justify-center group hover:(shadow-sm rounded-xl transition-shadow)">
+      <IconSauce class="fill-ninja-60 group-hover:fill-ninja-400 <sm:(w-[2.375rem] h-[2.375rem])" />
+      <div class="<sm:hidden text-base font-medium text-ninja-70">
         Соусы
       </div>
     </li>
