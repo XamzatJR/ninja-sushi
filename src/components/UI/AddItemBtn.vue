0cfadad88d06a83ec89c68d3a68807c12947f04d
XamzatJR <bahaev.hamzat95@gmail.com>
2023-03-26 00:15:14 +0300

    initial commit

---
+++ src/components/UI/AddItemBtn.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import IconPlus from '@/components/icons/IconPlus.vue'
+</script>
+
+<template>
+  <button class="py-1.5 px-3.5  sm:(px-6 py-2.5) bg-ninja-270 rounded-xl">
+    <IconPlus class="fill-ninja-250" />
+  </button>
+</template>
+
+<style scoped>
+
+</style>
