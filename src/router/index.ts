2c700249f0c2a5446567847ded17ffb253efec6b
XamzatJR <bahaev.hamzat95@gmail.com>
2023-03-26 00:24:33 +0300

    feat: update router to add new route for error page (NotFound)

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,9 +2,15 @@ import { createRouter, createWebHistory } from 'vue-router'
 const routes = [
   { path: '/', name: 'home', component: () => import('@/views/Home.vue') },
   { path: '/about', name: 'about', component: () => import('@/views/About.vue') },
+  { path: '/profile', name: 'profile', component: () => import('@/views/Profile.vue') },
   { path: '/card', name: 'card', component: () => import('@/views/Card.vue') },
   { path: '/product/:id', name: 'product', component: () => import('@/views/Product.vue') },
-  
+  {
+    path: '/:catchAll(.*)',
+    name: 'NotFound',
+    component: import('@/views/NotFound.vue')
+  }
+
 ]
 
 const router = createRouter({
