d594c047cadbd9431a825173d08afd355280aa6d
XamzatJR <bahaev.hamzat95@gmail.com>
2023-03-27 03:14:51 +0300

    feat(OrderHistory): add skeleton content

--- src/components/OrderHistory.vue
+++ src/components/OrderHistory.vue
@@ -1,7 +1,14 @@
 <template>
-    <div>
-
-    </div>
+    <section class="flex-grow">
+        <h2 class="font-bold text-5xl text-ninja-70 mb-6">История заказов</h2>
+        <div class="bg-white p-10 flex items-center gap-8 rounded-xl">
+            <img src="/order-history.png" alt="Order history picture">
+            <div class="">
+                <div class="font-medium text-3.5xl text-ninja-70 mb-2">У вас нет заказов</div>
+                <p class="text-ninja-100 text-base">Переходите в интересующую вас категорию <br> и сделайте свой первый заказ</p>
+            </div>
+        </div>
+    </section>
 </template>
 
 <script setup lang="ts">
