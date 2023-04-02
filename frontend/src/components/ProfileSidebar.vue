1fd78cd1ba5e7ffc3a7a77850934b2e2695cc942
XamzatJR <bahaev.hamzat95@gmail.com>
2023-04-02 03:02:44 +0300

    fix: add tab_active class to right button

--- frontend/src/components/ProfileSidebar.vue
+++ frontend/src/components/ProfileSidebar.vue
@@ -6,7 +6,7 @@ import IconArchive from '@/components/icons/IconArchive.vue'
 import IconHeart from '@/components/icons/IconHeart.vue'
 import IconLocation from '@/components/icons/IconLocation.vue'
 import IconSun from '@/components/icons/IconSun.vue'
-import { useRoute, useRouter } from 'vue-router'
+import { useRoute } from 'vue-router'
 
 const props = withDefaults(defineProps<{
   name: string
@@ -17,26 +17,24 @@ const props = withDefaults(defineProps<{
   email: 'Lifetv277@gmaIl.com',
   phoneNumber: '+380976986848',
 })
-const activeTab = ref(0)
-let route = useRoute()
-console.log(route.redirectedFrom);
-console.log(route.params);
-console.log(route.query);
-console.log(route);
+
+const {name} = useRoute()
+const activeTab = ref(name)
+
 
 const tabs = [
-  { id: 0, icon: IconArchive, text: 'История Заказов', link: '/profile/history' },
-  { id: 1, icon: IconHeart, text: 'Избранные товары', link: '/profile/favorite' },
-  { id: 2, icon: IconLocation, text: 'Адрес доставки', link: '/profile/shipping' },
-  { id: 3, icon: IconSun, text: 'Тема сайта', link: '/profile/theme' },
+  { id: 0, icon: IconArchive, text: 'История Заказов', name: 'history' },
+  { id: 1, icon: IconHeart, text: 'Избранные товары', name: 'favorite' },
+  { id: 2, icon: IconLocation, text: 'Адрес доставки', name: 'shipping' },
+  { id: 3, icon: IconSun, text: 'Тема сайта', name: undefined },
 ]
 </script>
 
 <template>
   <aside class="flex flex-col gap-4">
     <div class="flex flex-col gap-3">
-      <router-link v-for="{ icon, text, id, link } in tabs" :key="id" :to="link" class="py-2.5 pl-3 pr-5 bg-transparent flex items-center rounded-xl gap-3 group" :class="{ tab_active: id === activeTab, btn_mdfs: id !== activeTab }" @click="() => activeTab = id">
-        <div class="p-2.5 rounded-xl bg-white border border-white" :class="{ 'group-hover:(bg-ninja-200 border-ninja-200)': id !== activeTab }">
+      <router-link v-for="{ icon, text, id, name } in tabs" :key="id" :to="{name}" class="py-2.5 pl-3 pr-5 bg-transparent flex items-center rounded-xl gap-3 group" :class="{ tab_active: name === activeTab, btn_mdfs: name !== activeTab }" @click="() => activeTab = name">
+        <div class="p-2.5 rounded-xl bg-white border border-white" :class="{ 'group-hover:(bg-ninja-200 border-ninja-200)': name !== activeTab }">
           <component :is="icon" class="fill-ninja-70" />
         </div>
         <div class="text-base font-semibold">
