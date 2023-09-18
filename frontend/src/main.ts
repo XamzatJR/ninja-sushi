bf50139d495d6df0590ff780a1ef2424ec160959
XamzatJR <bahaev.hamzat95@gmail.com>
2023-09-18 13:59:33 +0300

    feat: add vue-query

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,12 +2,26 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import 'virtual:windi.css'
 
+import { VueQueryPlugin, type VueQueryPluginOptions } from '@tanstack/vue-query'
 import App from './App.vue'
 import router from './router'
+
+const vueQueryOptions: VueQueryPluginOptions = {
+  queryClientConfig: {
+    defaultOptions: {
+      queries: {
+        retry: 1,
+        refetchOnWindowFocus: false,
+        staleTime: 80000,
+      },
+    },
+  },
+}
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(VueQueryPlugin, vueQueryOptions)
 
 app.mount('#app')
