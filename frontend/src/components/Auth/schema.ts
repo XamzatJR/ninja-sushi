a207786fa27a762cc3d04492c4fe3fdd2d46bf6c
XamzatJR <bahaev.hamzat95@gmail.com>
2023-09-21 11:29:58 +0300

    fix: update regex

--- frontend/src/components/Auth/schema.ts
+++ frontend/src/components/Auth/schema.ts
@@ -5,7 +5,7 @@ export const registerSchema = z.object({
   password: z.string({ required_error: 'Email не указан' }).nonempty('Пароль не указан')
     .min(6, 'Пароль должен быть меньше 6 символов')
     .max(24, 'Пароль должен быть больше 24 символов')
-    .regex(/\w+?/, 'Неверный пароль'),
+    .regex(/^[a-zA-Z0-9_]*$/, 'Неверный пароль'),
 })
 
 export const loginSchema = z.object({
