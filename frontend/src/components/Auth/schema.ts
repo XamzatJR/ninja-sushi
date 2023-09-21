f19a0d31db14c78c59d177026f4687aabbea3525
XamzatJR <bahaev.hamzat95@gmail.com>
2023-09-21 11:29:18 +0300

    feat: add auth validation scheme

---
+++ frontend/src/components/Auth/schema.ts
@@ -0,0 +1,17 @@
+import { z } from 'zod'
+
+export const registerSchema = z.object({
+  email: z.string({ required_error: 'Email не указан' }).nonempty('Email не указан').email('Неправильный email'),
+  password: z.string({ required_error: 'Email не указан' }).nonempty('Пароль не указан')
+    .min(6, 'Пароль должен быть меньше 6 символов')
+    .max(24, 'Пароль должен быть больше 24 символов')
+    .regex(/\w+?/, 'Неверный пароль'),
+})
+
+export const loginSchema = z.object({
+  email: z.string({ required_error: 'Email не указан' }).nonempty('Email не указан').email('Неправильный email'),
+  password: z.string({ required_error: 'Email не указан' }).nonempty('Пароль не указан')
+    .min(6, 'Пароль должен быть меньше 6 символов')
+    .max(24, 'Пароль должен быть больше 24 символов')
+    .regex(/^[a-zA-Z0-9_]*$/, 'Неверный пароль'),
+})
