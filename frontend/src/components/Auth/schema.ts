80c93959cf75cc174159dce541ba5231eaaf640d
XamzatJR <bahaev.hamzat95@gmail.com>
2023-09-21 12:00:09 +0300

    fix: fix password required message

--- frontend/src/components/Auth/schema.ts
+++ frontend/src/components/Auth/schema.ts
@@ -2,7 +2,7 @@ import { z } from 'zod'
 
 export const registerSchema = z.object({
   email: z.string({ required_error: 'Email не указан' }).nonempty('Email не указан').email('Неправильный email'),
-  password: z.string({ required_error: 'Email не указан' }).nonempty('Пароль не указан')
+  password: z.string({ required_error: 'Пароль не указан' }).nonempty('Пароль не указан')
     .min(6, 'Пароль должен быть меньше 6 символов')
     .max(24, 'Пароль должен быть больше 24 символов')
     .regex(/^[a-zA-Z0-9_]*$/, 'Неверный пароль'),
@@ -10,7 +10,7 @@ export const registerSchema = z.object({
 
 export const loginSchema = z.object({
   email: z.string({ required_error: 'Email не указан' }).nonempty('Email не указан').email('Неправильный email'),
-  password: z.string({ required_error: 'Email не указан' }).nonempty('Пароль не указан')
+  password: z.string({ required_error: 'Пароль не указан' }).nonempty('Пароль не указан')
     .min(6, 'Пароль должен быть меньше 6 символов')
     .max(24, 'Пароль должен быть больше 24 символов')
     .regex(/^[a-zA-Z0-9_]*$/, 'Неверный пароль'),
