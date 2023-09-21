8d98f21f84cd4f879cbc58960def0e11c370c4a4
XamzatJR <bahaev.hamzat95@gmail.com>
2023-09-21 14:34:18 +0300

    fix: update min-max messages

--- frontend/src/components/Auth/schema.ts
+++ frontend/src/components/Auth/schema.ts
@@ -3,15 +3,15 @@ import { z } from 'zod'
 export const registerSchema = z.object({
   email: z.string({ required_error: 'Email не указан' }).nonempty('Email не указан').email('Неправильный email'),
   password: z.string({ required_error: 'Пароль не указан' }).nonempty('Пароль не указан')
-    .min(6, 'Пароль должен быть меньше 6 символов')
-    .max(24, 'Пароль должен быть больше 24 символов')
+    .min(6, 'Пароль должен быть не меньше 6 символов')
+    .max(24, 'Пароль должен быть не больше 24 символов')
     .regex(/^[a-zA-Z0-9_]*$/, 'Неверный пароль'),
 })
 
 export const loginSchema = z.object({
   email: z.string({ required_error: 'Email не указан' }).nonempty('Email не указан').email('Неправильный email'),
   password: z.string({ required_error: 'Пароль не указан' }).nonempty('Пароль не указан')
-    .min(6, 'Пароль должен быть меньше 6 символов')
-    .max(24, 'Пароль должен быть больше 24 символов')
+    .min(6, 'Пароль должен быть не меньше 6 символов')
+    .max(24, 'Пароль должен быть не больше 24 символов')
     .regex(/^[a-zA-Z0-9_]*$/, 'Неверный пароль'),
 })
